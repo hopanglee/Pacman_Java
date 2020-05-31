d4961efbcd4ea532ed18bbd5e0d41f0c45096714
HopangLee <hgknim87j@naver.com>
2020-05-31 18:52:36 +0900

    add BigCoin class, fix Pacman class by adding variable

---
+++ PacMan_java/src/BigCoin.java
@@ -0,0 +1,8 @@
+
+public class BigCoin extends Coin{
+
+	public BigCoin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- PacMan_java/src/Pacman.java
+++ PacMan_java/src/Pacman.java
@@ -1,8 +1,14 @@
 
 public class Pacman extends GameObject{
 
+	public int direction = 0; // 팩맨의 이동 방향 (0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3:위쪽)
+	
+	private int speed = 4; // 팩맨 이동속도
+	private int imageindex = 0; // 팩맨 이미지 index (0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽)
+	private int animation_index = 0; // 팩맨 애니메이션 index
+	
 	public Pacman() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
